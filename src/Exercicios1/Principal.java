611821d98b2f5a5c6c6f5ab7df3b695756244fcf
Rui <Rui@192.168.1.7>
2016-11-20 22:23:39 +0000

    asdas

--- src/Exercicios1/Principal.java
+++ src/Exercicios1/Principal.java
@@ -2,13 +2,35 @@ package Exercicios1;
 import Exercicios2.*;
 import Exercicios3.*;
 
+
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		new CriarArtigo();
+		System.out.println("-----------------Escolha um opção----------------- ");
+		System.out.println("1-Calculadora de Gastos");
+		System.out.println("2-Media de Golos ");
+		System.out.println("3-Compras");
+		System.out.println("0-Sair");
+		
+		Scanner reader = new Scanner(System.in);
+		int op=reader.nextInt();
+		
+		switch(op){
+		case 1 : new MediaDespesas();break; 
+		case 2 : new Jogo();break;
+		case 3 : new CriarArtigo();break;
+		case 0 : System.out.println("Adeus!!");break;
+		default: System.out.println("Valor invalido");break;
+		
+		
+		
+		
+		
+		
+		}
 
 	}
 
